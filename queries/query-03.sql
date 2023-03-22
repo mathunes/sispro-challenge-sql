5e76f9bdcd1f2a079815371894d985460385a3a3
Matheus Vieira <matheus.vieira@primeup.com.br>
2023-03-21 21:53:02 -0300

    fix query 3

--- queries/query-03.sql
+++ queries/query-03.sql
@@ -6,5 +6,6 @@ FROM
     Usuario u
 INNER JOIN Identificacao i ON u.id_identificacao = i.id_identificacao
 INNER JOIN Carona c ON u.id_usuario = c.id_usuario
+INNER JOIN Parada p ON c.id_carona = p.id_carona
 AND c.passageiros >= 2
-AND c.destino = 'Plaza Shopping';
+AND p.descricao = 'Plaza Shopping';
