a9ae74dc00ba73cddb3f463c6ef8e696cb0d3aab
Matheus Vieira <matheus.vieira@primeup.com.br>
2023-03-21 21:50:18 -0300

    fix Usuario table Identificacao fk

--- tables/Usuario.sql
+++ tables/Usuario.sql
@@ -2,5 +2,7 @@ CREATE TABLE Usuario (
     id_usuario INT,
     id_identificacao INT,
     admin BOOLEAN,
-    CONSTRAINT id_usuario_pk PRIMARY KEY (id_usuario)
+    CONSTRAINT id_usuario_pk PRIMARY KEY (id_usuario),
+    CONSTRAINT id_usuario_identificacao_fk FOREIGN KEY (id_identificacao)
+    REFERENCES Identificacao(id_identificacao)
 )
